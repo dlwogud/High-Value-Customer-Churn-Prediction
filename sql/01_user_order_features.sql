e621e1ab519d9d99b97b23693267b9246b30f3a2
dlwogud <89535380+dlwogud@users.noreply.github.com>
2026-04-03 23:02:01 +0900

    Update user order features SQL query
    Refactor SQL to simplify average calculation and add a select statement.

--- sql/01_user_order_features.sql
+++ sql/01_user_order_features.sql
@@ -5,16 +5,13 @@ CREATE OR REPLACE TABLE `instacart-churn.instacart_ds.user_order_features` AS
 SELECT
   user_id,
   COUNT(*) AS total_orders,
-  COALESCE(
-    AVG(
-      CASE
-        WHEN days_since_prior_order IS NOT NULL
-         AND days_since_prior_order > 0
-        THEN days_since_prior_order
-      END
-    ),
-    0
-  ) AS avg_days_between_orders,
+  AVG(CASE 
+        WHEN days_since_prior_order IS NOT NULL THEN days_since_prior_order 
+      END) AS avg_days_between_orders,
   MAX(order_number) AS max_order_number
 FROM `instacart-churn.instacart_ds.orders`
 GROUP BY user_id;
+
+SELECT *
+FROM `instacart-churn.instacart_ds.user_order_features`
+LIMIT 10;
