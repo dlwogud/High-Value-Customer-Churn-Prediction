255efefa0dcebe3ad5f546bccfe83ef9488226fc
dlwogud <89535380+dlwogud@users.noreply.github.com>
2026-04-07 16:00:09 +0900

    Update 02_high_value_customers.sql

--- sql/02_high_value_customers.sql
+++ sql/02_high_value_customers.sql
@@ -1,14 +1,17 @@
--- 목적: 고가치 고객 정의
--- 설명: 주문 횟수 기준 상위 20% 고객을 고가치 고객으로 추출
+-- 목적: 고빈도 고객 정의 + threshold 숫자 저장
+-- 설명:
+-- 1) 전체 고객의 total_orders 분포에서 상위 20% 기준 추출
+-- 2) 실제 threshold 값을 별도 테이블에 저장
+-- 3) 발표 시 "상위 20%"와 "N회 이상"을 함께 설명 가능하도록 구성
+
+CREATE OR REPLACE TABLE `instacart-churn.instacart_ds.high_value_threshold` AS
+SELECT
+  APPROX_QUANTILES(total_orders, 100)[OFFSET(80)] AS order_threshold
+FROM `instacart-churn.instacart_ds.user_order_features_all`;
 
 CREATE OR REPLACE TABLE `instacart-churn.instacart_ds.high_value_customers` AS
-WITH threshold_table AS (
-  SELECT
-    APPROX_QUANTILES(total_orders, 100)[OFFSET(80)] AS order_threshold
-  FROM `instacart-churn.instacart_ds.user_order_features`
-)
 SELECT
-  u.*
-FROM `instacart-churn.instacart_ds.user_order_features` AS u
-CROSS JOIN threshold_table AS t
-WHERE u.total_orders >= t.order_threshold;
+  f.*
+FROM `instacart-churn.instacart_ds.user_order_features_all` f
+CROSS JOIN `instacart-churn.instacart_ds.high_value_threshold` t
+WHERE f.total_orders >= t.order_threshold;
