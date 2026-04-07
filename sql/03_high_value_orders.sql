23133aa1ebf98de69ec9af1d08540edea8f0db73
dlwogud <89535380+dlwogud@users.noreply.github.com>
2026-04-07 16:02:47 +0900

    Update 03_high_value_orders.sql

--- sql/03_high_value_orders.sql
+++ sql/03_high_value_orders.sql
@@ -1,9 +1,11 @@
--- 목적: 고가치 고객의 주문 데이터만 추출
--- 설명: 전체 주문 데이터에서 고가치 고객의 주문 내역만 필터링
+-- 목적: 고빈도 고객 전체 주문 이력 추출
+-- 설명:
+-- 이후 단계에서 target order와 history order를 분리할 수 있도록
+-- 고빈도 고객의 전체 주문 데이터를 유지
 
 CREATE OR REPLACE TABLE `instacart-churn.instacart_ds.high_value_orders` AS
 SELECT
   o.*
-FROM `instacart-churn.instacart_ds.orders` AS o
-JOIN `instacart-churn.instacart_ds.high_value_customers` AS h
+FROM `instacart-churn.instacart_ds.orders` o
+JOIN `instacart-churn.instacart_ds.high_value_customers` h
   ON o.user_id = h.user_id;
